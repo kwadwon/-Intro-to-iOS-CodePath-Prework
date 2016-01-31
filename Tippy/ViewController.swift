66b5072ac6adeec519b8b187ab45b956bf50a239
Kwadwo Nyarko <kwadwon@kwadwos-macbook-pro.local>
2016-01-31 13:14:29 -0800

    Initial Commit

---
+++ Tippy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by Kwadwo Nyarko on 1/31/16.
+//  Copyright © 2016 Kwadwo Nyarko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
